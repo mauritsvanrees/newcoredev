f6817d8709aa5a428490394f64dae4f65c310b6f
Maurits van Rees <maurits@vanrees.org>
2023-05-19 00:30:57 +0200

    Add release/update_constraints.py as alternative to the shell version.

---
+++ release/update_constraints.py
@@ -0,0 +1,17 @@
+#! /bin/sh
+# Pip freeze the installed packages into constraints.txt.
+# Note that pip/setuptools/wheel are excluded by pip unless we call it with --all.
+# We are happy to follow pip's lead here.
+# We also exclude zc.buildout and our dummy plonecoredev package.
+# And we exclude editable packages.
+import subprocess
+
+
+process = subprocess.run(
+    ["bin/pip", "freeze", "--exclude-editable", "--exclude", "plonecoredev", "--exclude", "zc.buildout"],
+    check=True,
+    capture_output=True,
+    text=True,
+)
+with open("constraints.txt", "w") as myfile:
+    myfile.write("\n".join(sorted(process.stdout.splitlines(), key=str.lower)))
