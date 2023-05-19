fb7450ffecae86af13e74a8a60918c32d0ec4ea6
Maurits van Rees <maurits@vanrees.org>
2023-05-19 11:19:12 +0200

    Removed shell scripts, we use only Python scripts now.

--- release/update_constraints.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# Pip freeze the installed packages into constraints.txt.
-# Note that pip/setuptools/wheel are excluded by pip unless we call it with --all.
-# We are happy to follow pip's lead here.
-# We also exclude zc.buildout and our dummy plonecoredev package.
-# And we exclude editable packages.
-bin/pip freeze --exclude-editable --exclude plonecoredev --exclude zc.buildout | sort --ignore-case > constraints.txt
